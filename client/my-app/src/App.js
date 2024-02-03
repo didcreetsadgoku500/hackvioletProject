4039617d879e75d645fb063dedf66a42fa3d1a56
Alex Peters <alexp@Alexs-MacBook-Pro.local>
2024-02-03 16:04:41 -0500

    Initial design

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -2,25 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/joy/Button';
-
+import Dropdown from '@mui/joy/Dropdown';
+import Menu from '@mui/joy/Menu';
+import MenuButton from '@mui/joy/MenuButton';
+import MenuItem from '@mui/joy/MenuItem';
+ 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button size='lg'>Hello World</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Dropdown style={{ float: 'left'}}>
+          <MenuButton style={{ backgroundColor: 'white', margin: '1rem'}}>Settings:</MenuButton>
+          <Menu>
+            <MenuItem>Edit Contact Info</MenuItem>
+            <MenuItem>Delete Data</MenuItem>
+          </Menu>
+        </Dropdown>
       </header>
+      <body style={{ height: '100vh', margin: 0, padding: 0 }}>
+        
+        {/* Need an introduction to the SOS system, and a button that appears front and center that users will press */}
+        <h1>Press the button to send an SOS</h1>
+        <p>You will be able to instantly chat with someone while awaiting emergency services.</p>
+        {/* <Button size='lg' color='danger'>SOS</Button> */}
+        <br />
+        <Button size='lg' className="circle-button"><img src={logo} className="App-logo" alt="logo" /></Button>
+      </body>
     </div>
   );
 }
