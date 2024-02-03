49cd046e6045c78bc8b72b8272c888fbdb42eded
jared.wal.20 <jared.wal.20@cnu.edu>
2024-02-03 13:47:50 -0500

    get added

--- server/index.js
+++ server/index.js
@@ -1,27 +1,41 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const dotenv = require('dotenv').config();
+
+const UserModel = require("./models/Users")
+
+const password = process.env.PASSWORD;
 
 app.listen(3005, ()=>{console.log("running")})
 //TODO environment password
-mongoose.connect('mongodb+srv://jared_w:<password>@emergency-contacts.67ictpk.mongodb.net/emergency-contacts?retryWrites=true&w=majority');
-const express = require('express');
-const app = express();
+
+mongoose.connect(`mongodb+srv://jared_w:${password}@emergency-contacts.67ictpk.mongodb.net/emergency-contacts?retryWrites=true&w=majority`);
 
 
 
 
+app.get("/getUser/:userId", async (req, res) => {
+    const userId = req.params.userId;
+    console.log("start");
 
+    try {
 
-
-// get emergency contact
-app.get("/....", async (req, res) => {
-
+        const user = await UserModel.findById(userId);
+        console.log(userId);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        console.log("user");
+        res.json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
-// Initial Registration
-    // send req=contact_info -> res=token
-app.post("/createUser", async (req,res) => {
+
+// app.post("/createUser", async (req,res) => {
 
 
-});
+// });
