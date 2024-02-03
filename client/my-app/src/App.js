839343efba708f3f2b3603cbc830ea5f6e146870
Alex Englert <alex.englert.22@cnu.edu>
2024-02-03 14:58:16 -0500

    Created pages

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,29 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Location from './Location';
+import appPage from './appPage/appPage';
+import trackerPage from './trackerPage/trackerPage';
 
 
 function App() {
+  let url = window.location.pathname
+
+
+  let page = (url == "/") ? appPage() : trackerPage();
+  console.log(url)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-        <Location />
-
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {page}
       </header>
     </div>
   );
